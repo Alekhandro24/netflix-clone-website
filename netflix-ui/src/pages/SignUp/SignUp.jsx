351b62f9bc151c69d07ef87983da98f4f7540d4d
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-09-11 19:16:29 +0300

    Create SignUp.jsx

---
+++ netflix-ui/src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,84 @@
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import React, { useState } from "react";
+import { Container } from "./SignUp.styled";
+import BackgroundImage from "../../component/Background/BackgroundImage";
+import Header from "../../component/Header/Header";
+import { firebaseAuth } from "../../utils/firebase-config";
+import { useNavigate } from "react-router-dom";
+
+const SignUp = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const handleSignIn = async () => {
+    try {
+      const { email, password } = formValues;
+      await createUserWithEmailAndPassword(firebaseAuth, email, password);
+    } catch (error) {}
+  };
+
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (currentUser) navigate("/");
+  });
+
+  return (
+    <Container showPassword={showPassword}>
+      <BackgroundImage />
+      <div className="content">
+        <Header login />
+        <div className="body flex column a-center j-center">
+          <div className="text flex column">
+            <h1>Unlimited movies, TV shows and more.</h1>
+            <h4>Watch anywhere. Cancel anytime.</h4>
+            <h6>
+              Ready to watch? Enter your email to create or restart membership.
+            </h6>
+          </div>
+          <div className="form">
+            <input
+              type="email"
+              placeholder="Email address"
+              name="email"
+              value={formValues.email}
+              onChange={(e) =>
+                setFormValues({
+                  ...formValues,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            />
+
+            {showPassword && (
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={formValues.password}
+                onChange={(e) =>
+                  setFormValues({
+                    ...formValues,
+                    [e.target.name]: e.target.value,
+                  })
+                }
+              />
+            )}
+
+            {!showPassword && (
+              <button onClick={() => setShowPassword(true)}>Get Started</button>
+            )}
+          </div>
+          <button onClick={handleSignIn}>Sign Up</button>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default SignUp;
