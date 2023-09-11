be8c89511671607033e89823d06d80e1790edf8e
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-09-11 19:16:13 +0300

    Create Login.jsx

---
+++ netflix-ui/src/pages/Login/Login.jsx
@@ -0,0 +1,58 @@
+import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { Container } from "./Login.styled";
+import BackgroundImage from "../../component/Background/BackgroundImage";
+import Header from "../../component/Header/Header";
+import { firebaseAuth } from "../../utils/firebase-config";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(firebaseAuth, email, password);
+    } catch (error) {
+      console.log(error.code);
+    }
+  };
+
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (currentUser) navigate("/");
+  });
+
+  return (
+    <Container>
+      <BackgroundImage />
+      <div className="content">
+        <Header />
+        <div className="form-container flex column a-center j-center">
+          <div className="form flex column a-center j-center">
+            <div className="title">
+              <h3>Login</h3>
+            </div>
+            <div className="container flex column">
+              <input
+                type="text"
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+              />
+              <button onClick={handleLogin}>Login to your account</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Login;
