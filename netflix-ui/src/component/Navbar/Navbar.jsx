b6c91a45edff1ea0590f4bd144444775ae5c94bf
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-09-11 19:15:33 +0300

    Create Navbar.jsx

---
+++ netflix-ui/src/component/Navbar/Navbar.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Container } from "./Navbar.styled";
+import logo from "../../assets/logo.png";
+import { Link, useNavigate } from "react-router-dom";
+import { FaPowerOff, FaSearch } from "react-icons/fa";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { firebaseAuth } from "../../utils/firebase-config";
+
+const Navbar = ({ isScrolled }) => {
+  const [showSearch, setShowSearch] = useState(false);
+  const [inputHover, setInputHover] = useState(false);
+  const navigate = useNavigate();
+
+  const links = [
+    { name: "Home", link: "/" },
+    { name: "TV Shows", link: "/tv" },
+    { name: "Movies", link: "/movies" },
+    { name: "My List", link: "/mylist" },
+  ];
+
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (!currentUser) navigate("/login");
+  });
+
+  return (
+    <Container>
+      <nav className={`flex ${isScrolled ? "scroll" : ""}`}>
+        <div className="left flex a-center">
+          <div className="brand flex a-center j-center">
+            <img src={logo} alt="logo" />
+          </div>
+          <ul className="links flex">
+            {links.map(({ name, link }) => {
+              return (
+                <li key={name}>
+                  <Link to={link}>{name}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="right flex a-center">
+          <div className={`search ${showSearch ? "show-search" : ""}`}>
+            <button
+              onFocus={() => setShowSearch(true)}
+              onBlur={() => {
+                if (!inputHover) setShowSearch(false);
+              }}
+            >
+              <FaSearch />
+            </button>
+            <input
+              type="text"
+              placeholder="Search"
+              onMouseEnter={() => setInputHover(true)}
+              onMouseLeave={() => setInputHover(false)}
+              onBlur={() => {
+                setShowSearch(false);
+                setInputHover(false);
+              }}
+            />
+          </div>
+          <button onClick={() => signOut(firebaseAuth)}>
+            <FaPowerOff />
+          </button>
+        </div>
+      </nav>
+    </Container>
+  );
+};
+
+export default Navbar;
