642109b6f3541e701cd2f2d575417699ff1e3389
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-09-14 16:58:04 +0300

    Create SelectGenre.styled.js

---
+++ netflix-ui/src/component/SelectGenre copy/SelectGenre.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Select = styled.select`
+  margin-left: 5rem;
+  background-color: rgba(0, 0, 0, 0.4);
+  color: white;
+
+  font-size: 1.4rem;
+  cursor: pointer;
+`;
