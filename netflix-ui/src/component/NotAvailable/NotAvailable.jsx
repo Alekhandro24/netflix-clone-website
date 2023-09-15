d7ddc3e88e0e3d39d1f35240830a584316633581
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-09-15 15:16:02 +0300

    Create NotAvailable.jsx

---
+++ netflix-ui/src/component/NotAvailable/NotAvailable.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NotAvailable = () => {
+  return (
+    <h1 className="not-available">No Movies avaialable for selected genre</h1>
+  );
+};
+
+export default NotAvailable;
