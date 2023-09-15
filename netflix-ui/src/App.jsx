cfa8a928cbc98b2633438f7c09024fa72ac19358
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-09-15 15:15:51 +0300

    Update App.jsx

--- netflix-ui/src/App.jsx
+++ netflix-ui/src/App.jsx
@@ -4,6 +4,8 @@ import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
 import Netflix from "./pages/Netflix/Netflix";
 import Player from "./pages/Player/Player";
+import Movies from "./pages/Movies/Movies";
+import TVShows from "./pages/TVShows/TVShows";
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<SignUp />} />
         <Route exact path="/player" element={<Player />} />
+        <Route exact path="/tv" element={<TVShows />} />
+        <Route exact path="/movies" element={<Movies />} />
         <Route exact path="/" element={<Netflix />} />
       </Routes>
     </>
