4bd3a4b338bf03f06fea0260276886012d8cd41e
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-09-15 15:16:16 +0300

    Create TVShows.styled.js

---
+++ netflix-ui/src/pages/TVShows/TVShows.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  .data {
+    margin-top: 8rem;
+    .not-available {
+      margin-top: 4rem;
+      text-align: center;
+      color: white;
+    }
+  }
+`;
