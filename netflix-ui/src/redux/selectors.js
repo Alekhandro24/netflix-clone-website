d2ef7a11a622d9a218943a6d9514a37b8f2f278c
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-09-15 15:15:53 +0300

    Update selectors.js

--- netflix-ui/src/redux/selectors.js
+++ netflix-ui/src/redux/selectors.js
@@ -1,2 +1,3 @@
 export const selectGenresLoaded = (state) => state.netflix.genresLoaded;
 export const selectMovies = (state) => state.netflix.movies;
+export const selectGenres = (state) => state.netflix.genres;
