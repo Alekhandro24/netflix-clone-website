2d47eb62b797d131de35b58851227a115e3c7248
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-09-15 15:15:41 +0300

    Delete NotAvailable.jsx

--- netflix-ui/src/component/NotAvailabl copy/NotAvailable.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const NotAvailable = () => {
-  return (
-    <h1 className="not-available">No Movies avaialable for selected genres</h1>
-  );
-};
-
-export default NotAvailable;
