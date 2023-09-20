d399cd3d09d27074cb08100a10cbf89a783a64b6
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-09-20 21:55:40 +0300

    Create UserListedMovies.jsx

---
+++ netflix-ui/src/pages/UserListedMovies/UserListedMovies.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { selectMovies } from "../../redux/selectors";
+import { getUserLikedMovies } from "../../redux/thunk";
+import { onAuthStateChanged } from "firebase/auth";
+import { firebaseAuth } from "../../utils/firebase-config";
+import { Container } from "./UserListedMovies.styled";
+import Navbar from "../../component/Navbar/Navbar";
+import Card from "../../component/Card/Card";
+
+const UserListedMovies = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [email, setEmail] = useState(undefined);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const movies = useSelector(selectMovies);
+
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (currentUser) setEmail(currentUser.email);
+    else navigate("/login");
+  });
+
+  useEffect(() => {
+    if (email) {
+      dispatch(getUserLikedMovies(email));
+    }
+  }, [dispatch, email]);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
+  return (
+    <Container>
+      <Navbar isScrolled={isScrolled} />
+      <div className="content flex column">
+        <h1>My List</h1>
+        <div className="grid flex">
+          {movies.map((movie, index) => {
+            return (
+              <Card
+                movieData={movie}
+                index={index}
+                key={movie.id}
+                isLiked={true}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default UserListedMovies;
