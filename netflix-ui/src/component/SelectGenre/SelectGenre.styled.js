133654ca86da6284a2511f37d5c6830bf4c9d245
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-09-20 21:54:49 +0300

    Update SelectGenre.styled.js

--- netflix-ui/src/component/SelectGenre/SelectGenre.styled.js
+++ netflix-ui/src/component/SelectGenre/SelectGenre.styled.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const Select = styled.select`
   margin-left: 5rem;
-  background-color: rgba(0, 0, 0, 0.4);
   color: white;
+  background-color: rgba(0, 0, 0, 0.4);
   font-size: 1.4rem;
   cursor: pointer;
 `;
