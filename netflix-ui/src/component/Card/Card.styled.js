de92c030cc515c4091db4a22b7dbc286550a71be
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-09-20 21:54:28 +0300

    Update Card.styled.js

--- netflix-ui/src/component/Card/Card.styled.js
+++ netflix-ui/src/component/Card/Card.styled.js
@@ -1,63 +1,54 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  position: relative;
   max-width: 230px;
   width: 230px;
-  height: 100px;
+  height: 100%;
   cursor: pointer;
-
+  position: relative;
   img {
+    border-radius: 0.2rem;
     width: 100%;
     height: 100%;
-
-    border-radius: 0.2rem;
     z-index: 10;
   }
-
   .hover {
+    z-index: 99;
+    height: max-content;
+    width: 20rem;
     position: absolute;
     top: -18vh;
     left: 0;
-    height: max-content;
-    width: 20rem;
     border-radius: 0.3rem;
-    box-shadow: rgba(0, 0, 0, 0.5) 0px 3px 10px;
+    box-shadow: rgba(0, 0, 0, 0.75) 0px 3px 10px;
     background-color: #181818;
     transition: 0.3s ease-in-out;
-    z-index: 90;
-
     .image-video-container {
       position: relative;
       height: 140px;
-
       img {
-        position: absolute;
-        position: absolute;
-        top: 0;
         width: 100%;
         height: 140px;
         object-fit: cover;
         border-radius: 0.3rem;
-
-        z-index: 4;
-      }
-
-      video {
-        position: absolute;
         top: 0;
+        z-index: 4;
+        position: absolute;
+      }
+      video {
         width: 100%;
-        height: 1400;
+        height: 140px;
         object-fit: cover;
         border-radius: 0.3rem;
+        top: 0;
         z-index: 5;
+        position: absolute;
       }
     }
     .info-container {
       padding: 1rem;
       gap: 0.5rem;
     }
-
     .icons {
       .controls {
         display: flex;
@@ -65,9 +56,8 @@ export const Container = styled.div`
       }
       svg {
         font-size: 2rem;
-        transition: 0ms.3 ease-in-out;
         cursor: pointer;
-
+        transition: 0.3s ease-in-out;
         &:hover {
           color: #b8b8b8;
         }
@@ -76,10 +66,8 @@ export const Container = styled.div`
     .genres {
       ul {
         gap: 1rem;
-
         li {
           padding-right: 0.7rem;
-
           &:first-of-type {
             list-style-type: none;
           }
