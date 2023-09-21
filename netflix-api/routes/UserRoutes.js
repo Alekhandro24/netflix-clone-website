4fde59dbe3bc0aa3c95598d798656692161d2152
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-09-21 14:46:59 +0300

    Update UserRoutes.js

--- netflix-api/routes/UserRoutes.js
+++ netflix-api/routes/UserRoutes.js
@@ -2,12 +2,12 @@ const {
   addToLikedMovies,
   getLikedMovies,
   removeFromLikedMovies,
-} = require("../controllers/UserControllers");
+} = require("../controllers/UserController");
 
 const router = require("express").Router();
 
-router.post("/add", addToLikedMovies);
 router.get("/liked/:email", getLikedMovies);
-router.put("/delete", removeFromLikedMovies);
+router.post("/add", addToLikedMovies);
+router.put("/remove", removeFromLikedMovies);
 
 module.exports = router;
