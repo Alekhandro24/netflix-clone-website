a645fe374ddb981249c4b9a81b531d15c8ad037e
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-09-21 14:45:45 +0300

    Update UserModel.js

--- netflix-api/models/UserModel.js
+++ netflix-api/models/UserModel.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
+    required: true,
     unique: true,
-    require: true,
     max: 50,
   },
   likedMovies: Array,
