3162bd9e2454142cc4370bacbfcf0a3b69196634
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-09-30 12:22:30 +0300

    some step

--- netflix-ui/src/component/Slider/Slider.styled.js
+++ netflix-ui/src/component/Slider/Slider.styled.js
@@ -4,18 +4,32 @@ export const Container = styled.div``;
 
 //new
 export const ContainerRow = styled.div`
+  overflow-y: hidden;
+  overflow-x: scroll;
   .row__posters {
     display: flex;
-    overflow-y: hidden;
-    overflow-x: scroll;
+
     padding: 20px;
+
+    .ContainerCard {
+      .row__poster {
+        width: 100%;
+        max-height: 100px;
+        margin-right: 10px;
+        object-fit: contain;
+        translate: transform 450ms;
+        &:hover {
+          transform: scale(1.48);
+        }
+      }
+    }
   }
 
   .row__posters::-webkit-scrollbar {
     display: none;
   }
 
-  .row__poster {
+  /* .row__poster {
     width: 100%;
     max-height: 100px;
     margin-right: 10px;
@@ -25,5 +39,5 @@ export const ContainerRow = styled.div`
 
   .row__poster:hover {
     transform: scale(1.48);
-  }
+  } */
 `;
