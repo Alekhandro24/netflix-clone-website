da23e83ca1f2c52210af49c065f8396bebd683f9
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-09-30 03:11:29 +0300

    Update Slider.styled.js

--- netflix-ui/src/component/Slider/Slider.styled.js
+++ netflix-ui/src/component/Slider/Slider.styled.js
@@ -1,3 +1,29 @@
 import styled from "styled-components";
 
 export const Container = styled.div``;
+
+//new
+export const ContainerRow = styled.div`
+  .row__posters {
+    display: flex;
+    overflow-y: hidden;
+    overflow-x: scroll;
+    padding: 20px;
+  }
+
+  .row__posters::-webkit-scrollbar {
+    display: none;
+  }
+
+  .row__poster {
+    width: 100%;
+    max-height: 100px;
+    margin-right: 10px;
+    object-fit: contain;
+    translate: transform 450ms;
+  }
+
+  .row__poster:hover {
+    transform: scale(1.48);
+  }
+`;
