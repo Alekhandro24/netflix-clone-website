490dd47c82881535cabd5d327bb57c97ecac9037
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-10-02 21:59:49 +0300

    Update Slider.styled.js

--- netflix-ui/src/component/Slider/Slider.styled.js
+++ netflix-ui/src/component/Slider/Slider.styled.js
@@ -2,42 +2,90 @@ import styled from "styled-components";
 
 export const Container = styled.div``;
 
-//new
 export const ContainerRow = styled.div`
-  overflow-y: hidden;
-  overflow-x: scroll;
+  h2 {
+    margin-left: 50px;
+  }
+
   .row__posters {
     display: flex;
-
+    gap: 15px;
     padding: 20px;
+    overflow-y: hidden;
+    overflow-x: scroll;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
 
     .ContainerCard {
+      position: relative;
+      cursor: pointer;
+
       .row__poster {
-        width: 100%;
-        max-height: 100px;
-        margin-right: 10px;
+        width: 250px;
+        height: 145px;
+        border-radius: 0.2rem;
         object-fit: contain;
-        translate: transform 450ms;
-        &:hover {
-          transform: scale(1.48);
+        z-index: 10;
+      }
+
+      .hover {
+        position: absolute;
+        transform: scale(0.9);
+        z-index: 99;
+        top: -6vh;
+        left: 0;
+        border-radius: 0.3rem;
+        box-shadow: rgba(0, 0, 0, 0.75) 0px 3px 10px;
+        background-color: #181818;
+
+        .image-video-container {
+          position: relative;
+          width: 280px;
+          height: 135px;
+          overflow: hidden;
+
+          img {
+            width: 280px;
+            height: 135px;
+            object-fit: cover;
+            border-radius: 0.3rem;
+            top: 0;
+            z-index: 4;
+            position: absolute;
+          }
+        }
+        .info-container {
+          padding: 0.2rem;
+          gap: 0.2rem;
+        }
+        .icons {
+          .controls {
+            display: flex;
+            gap: 1rem;
+          }
+          svg {
+            font-size: 1rem;
+            cursor: pointer;
+            transition: 0.3s ease-in-out;
+            &:hover {
+              color: #b8b8b8;
+            }
+          }
+        }
+        .genres {
+          ul {
+            gap: 1.5rem;
+            li {
+              padding-right: 0.7rem;
+              &:first-of-type {
+                list-style-type: none;
+              }
+            }
+          }
         }
       }
     }
   }
-
-  .row__posters::-webkit-scrollbar {
-    display: none;
-  }
-
-  /* .row__poster {
-    width: 100%;
-    max-height: 100px;
-    margin-right: 10px;
-    object-fit: contain;
-    translate: transform 450ms;
-  }
-
-  .row__poster:hover {
-    transform: scale(1.48);
-  } */
 `;
